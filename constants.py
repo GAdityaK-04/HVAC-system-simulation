9137683c1e50f37ff05992f2cbdb56763696bf0a
G Aditya Kumar <akumar.gak@gmail.com>
2025-12-07 19:37:36 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,18 @@
+# setting and defining constants required
+
+C = 1.2e7
+Q_occ = 500
+Q_max = 15000
+Q_hvac = Q_max
+COP = 3.5
+dt = 300
+R = 0.003
+temp_change_list = [15]
+
+T_setpoint = 22
+T_high = 22.5
+T_low = 21.5
+T = 15
+
+R_rise = 6.02e-4
+R_fall = -5.90e-4
